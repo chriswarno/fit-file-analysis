8bddc0b279317372633d4b52d18773e5e57600ed
Chris Warno <cwarno95@gmail.com>
2025-01-24 01:00:29 -0500

    Added a temporary directory and put the fit file in it. Utilized Garmin fit SDK to create stream, decoder, messages, and errors.

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
 from garmin_fit_sdk import Decoder, Stream
+import tempfile
+import os
 
 app = FastAPI()
+
+uploads = tempfile.TemporaryDirectory()  # Temporary directory to store uploaded files
 
 
 @app.post("/uploadfile/")
 async def upload_file(file: Annotated[UploadFile, File()]):
     # Ensure the uploaded file is a .fit file
     if file.filename.endswith(".fit"):
-        # Read the file content as a stream
-        file_content = await file.read()
-        stream = Stream.from_file(file_content)  # Use from_bytes for the in-memory content
+        upload_directory = uploads.name
+        file_path = os.path.join(upload_directory, file.filename)
 
-        # Check if the stream is a valid .fit file
-        is_fit = Decoder.is_fit(stream)
-        return {"filename": file.filename, "is_fit": is_fit}
+        with open(file_path, "wb") as temp_file:
+            temp_file.write(await file.read())
+
+        uploaded_file = file_path
+        stream = Stream.from_file(uploaded_file)
+        decoder = Decoder(stream)
+        messages, errors = decoder.read()
+
+        return {"errors": errors, "messages": messages}
     
     return {"error": "Uploaded file is not a .fit file"}
 
