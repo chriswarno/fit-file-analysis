655c57d9a4754f338c690d0eced29979853438b4
Chris Warno <cwarno95@gmail.com>
2025-03-02 22:04:47 -0500

    fixed decoding and started adding ride data utilities

--- utils/fit_utils/fit_decode_util.py
+++ utils/fit_utils/fit_decode_util.py
@@ -6,23 +6,23 @@ from io import BufferedReader
 
 @st.cache_data
 def messages(file):
+    '''
     # elif statement to get the stream depending on how the file data was sent to the function
     if isinstance(file, str): # if a file path is provided
         stream = Stream.from_file(file)
-        #return stream
+        print("file path")
     elif isinstance(file, bytearray): # if a bytearray is provided
         stream = Stream.from_byte_array(file)
-        #return stream
+        print("bytearray")
     elif isinstance(file, bytes): # if a bytesIO object was provided
-        stream = Stream.from_byte_io(file)
-        #return stream
-    elif isinstance(file, BufferedReader): # if a BufferedReader was provided
+        stream = Stream.from_bytes_io(file)
+        print("bytesio")
+    else: # hopefully this works
         stream = Stream.from_buffered_reader(file)
-        #return stream
-    else: # if none of the functions work
-        e = st.error("An error occurred decoding the file.")
-        #return e
-
+        print("BufferedReader")
+    '''
+    stream = Stream.from_byte_array(file)
+        
     try:
         st.write(stream)
         
@@ -31,6 +31,8 @@ def messages(file):
         messages, errors = decoder.read(  # Read the fit file to a dict
             convert_datetimes_to_dates=True,
         )
+
+        st.write(messages)
 
         if errors:  # If there are errors in decoding the file, error message is returned
             e = "There were errors in decoding the file"
